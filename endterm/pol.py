c4c1faed469a2524601ae1180df3b8bf312c5a32
mo0lig <igalagik@gmail.com>
2023-12-02 23:01:45 +0600

    ю

--- endterm/pol.py
+++ endterm/pol.py
@@ -1,71 +1,82 @@
-def matrix_multiply(A, B):
- 
-    rows_A, cols_A = len(A), len(A[0])
-    rows_B, cols_B = len(B), len(B[0])
-
-    result = [[0 for _ in range(cols_B)] for _ in range(rows_A)]
-
-    for i in range(rows_A):
-        for j in range(cols_B):
-            for k in range(cols_A):
-                result[i][j] += A[i][k] * B[k][j]
-
-    return result
-def matrix_identity(size):
-
-    return [[1 if i == j else 0 for j in range(size)] for i in range(size)]
-
-def matrix_subtract(A, B):
-
-    rows, cols = len(A), len(A[0])
-
-    result = [[A[i][j] - B[i][j] for j in range(cols)] for i in range(rows)]
-
-    return result
-
-def matrix_determinant(matrix):
-
-    size = len(matrix)
-
-    if size == 1:
+import numpy as np
+def minor(matrix,i,j):
+    mat=list()
+    lis=[]
+    for k in range(len(matrix)):
+        if k==i:
+            continue
+        else:
+            for s in range(len(matrix)):
+                if s==j:
+                    continue
+                else:
+                    lis.append(matrix[k][s])
+            mat.append(lis)
+            lis=[]
+    return mat
+                
+def dt(matrix):
+    if len(matrix)==1:
         return matrix[0][0]
-
-    if size == 2:
-        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
-
-    determinant = 0
-    for i in range(size):
-        submatrix = [row[:i] + row[i + 1:] for row in matrix[1:]]
-        determinant += ((-1) ** i) * matrix[0][i] * matrix_determinant(submatrix)
-
-    return determinant
-
-def matrix_pow(matrix, power):
-
-    result = matrix_identity(len(matrix))
-    for _ in range(power):
-        result = matrix_multiply(result, matrix)
-
-    return result
-
-def characteristic_polynomial(matrix):
-    size = len(matrix)
-
-    lambda_identity = matrix_subtract(matrix, matrix_identity(size))
-
-    char_poly = [1]
-    for i in range(size):
-        factor = matrix_subtract(matrix_identity(size), matrix_pow(lambda_identity, i + 1))
-        char_poly = matrix_multiply(char_poly, factor)
-
-    return char_poly[0]
-
-matrix = [[1, 2], [3, 4]]  
-char_poly = characteristic_polynomial(matrix)
-
-print("Matrix:")
-for row in matrix:
-    print(row)
-print("\nCharacteristic Polynomial Coefficients:")
-print(char_poly)
-
+    if len(matrix)==2:
+        return matrix[0][0]*matrix[1][1] - matrix[0][1]*matrix[1][0]
+    det=0
+    for k in range(len(matrix)):
+        det += (matrix[0][k] * (-1)**k) * dt(minor(matrix,0,k))
+    return det
+def coef(matrix):
+    return [[((-1)**(i + j))*dt(minor(matrix,i,j)) for j in range(len(matrix[i]))] for i in range(len(matrix))]
+def polynom(matrix):
+    sum=0
+    det=dt(matrix)
+    for i in range(n):
+        for j in range(n):
+            if i==j:
+                sum+=matrix[i][j]
+    if n==2:
+        if det<0:
+            if sum>0:
+                return (f"t^2 - {sum}t - {abs(dt(matrix))}")
+            else:
+                return (f"t^2 + {abs(sum)}t - {abs(dt(matrix))}")
+        elif det>0:
+            if sum>0:
+                return (f"t^2 - {sum}t + {dt(matrix)}")
+            else:
+                return (f"t^2 + {sum}t + {dt(matrix)}")
+    if n==3:
+        cf=dt(coef(matrix))
+        if det<0:
+            if sum>0:
+                if cf>0:
+                    return f"t^3 - {sum}t^2 + {cf}t + {abs(dt(matrix))}"
+                else:
+                    return f"t^3 - {sum}t^2 - {abs(cf)}t + {abs(dt(matrix))}"   
+            else:
+                if cf>0:
+                    return f"t^3 + {abs(sum)}t^2 + {cf}t + {abs(dt(matrix))}"
+                else:
+                    return f"t^3 + {abs(sum)}t^2 - {abs(cf)}t + {abs(dt(matrix))}" 
+        elif det>0:
+            if sum>0:
+                if cf>0:
+                    return f"t^3 - {sum}t^2 + {cf}t - {dt(matrix)}"
+                else:
+                    return f"t^3 - {sum}t^2 - {abs(cf)}t - {dt(matrix)}"
+            else:
+                if cf>0:
+                    return f"t^3 + {abs(sum)}t^2 + {cf}t - {dt(matrix)}"
+                else:
+                    return f"t^3 + {abs(sum)}t^2 - {abs(cf)}t - {dt(matrix)}"
+        
+          
+n=int(input("nxn size of matrix:"))
+matrix=list()
+ll=[]
+for i in range(n):
+    for j in range(n):
+        x=int(input(f"[{i}][{j}]="))
+        ll.append(x)
+    matrix.append(ll)
+    ll=[]
+print(polynom(matrix))
